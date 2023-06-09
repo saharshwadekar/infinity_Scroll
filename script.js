9ab391ea50512d52cc435ae98c22b7f8ca0694cd
Saharsh-Wadekar <saharshwadekar@gmail.com>
2023-06-10 00:28:19 +0530

    finalized

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const loader = document.getElementById('loader');
 const imageContainer = document.getElementById('image-container');
 
 let requestedImageArray = [];
-
-loader.hidden = true;
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 
 function showLoadingPage()
 {
@@ -14,7 +15,7 @@ function removeLoadingPage()
     loader.hidden = true;
 }
 
-const initialCountImages = 10;
+const initialCountImages = 4;
 const apiAccessKey = `zmbuK6-ffjI4JIMy2fWU9Xgf8uk5UayxBx3_OQ2gMu4`; 
 
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiAccessKey}&count=${initialCountImages}`
@@ -30,6 +31,8 @@ function helpSetAttribute(htmlTag , attributeDictionary)
 function displayImages() 
 {
     showLoadingPage();
+    imagesLoaded = 0;
+    totalImages = requestedImageArray.length;
     requestedImageArray.forEach((image) => {
         const anchorTag = document.createElement('a');
         helpSetAttribute(anchorTag,
@@ -45,7 +48,15 @@ function displayImages()
                alt: image.alt_description,
                title: image.alt_description
             })
-
+        imageTag.addEventListener('load' , () =>
+        {
+            imagesLoaded++;
+            if(imagesLoaded === totalImages)
+            {
+                ready= true;
+                initialCountImages = 10;
+            }
+        })
         anchorTag.appendChild(imageTag);
         imageContainer.appendChild(anchorTag);
     });
@@ -70,8 +81,9 @@ getImagesFromAPI();
 
 window.addEventListener('scroll',() => 
 {
-    if(window.innerHeight + window.scrollY >= document.body.offsetHeight)
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready)
     {
+        ready = false;
         getImagesFromAPI();
     }
 })
